204341914f5133d5f902c60522d06be4d9cf0550
Vedant VN <81044922+Vedant-VN@users.noreply.github.com>
2021-09-30 18:35:57 +0530

    Sigma

--- bot.py
+++ bot.py
@@ -21,11 +21,11 @@ if __name__ == "__main__" :
         root="plugins"
     )
     app = pyrogram.Client(
-        "RenameBot",
+        "Hayasaka",
         bot_token=Config.TG_BOT_TOKEN,
         api_id=Config.APP_ID,
         api_hash=Config.API_HASH,
         plugins=plugins
     )
-    Config.AUTH_USERS.add(861055237)
+    Config.AUTH_USERS.add(1500531745)
     app.run()
