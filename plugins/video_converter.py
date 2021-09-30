5365da853f56f0694830427c1d6516a3229b34ee
Vedant VN <81044922+Vedant-VN@users.noreply.github.com>
2021-10-01 02:47:19 +0530

    Sigma

--- plugins/video_converter.py
+++ plugins/video_converter.py
@@ -50,7 +50,7 @@ async def convert_to_video(bot, update):
             await update.reply_text(
                 text="**Please Join My Anime Group Before Using Me..**",
                 reply_markup=InlineKeyboardMarkup([
-                    [ InlineKeyboardButton(text="Join My Anime Group", url=f"https://t.me/{update_channel}")]
+                    [ InlineKeyboardButton(text="❣ Join My Anime Group ❣", url=f"https://t.me/{update_channel}")]
               ])
             )
             return  
