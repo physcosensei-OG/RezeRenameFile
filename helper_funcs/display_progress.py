a0be3a944be8d9330610085cff6315f97c4cbe71
Vedant VN <81044922+Vedant-VN@users.noreply.github.com>
2021-09-30 17:34:07 +0530

    done

--- helper_funcs/display_progress.py
+++ helper_funcs/display_progress.py
@@ -21,7 +21,7 @@ async def progress_for_pyrogram(current, total, ud_type, message, start):
     reply_markup=InlineKeyboardMarkup(
             [
                 [
-                    InlineKeyboardButton("🚫Cancel", callback_data = "closeme")
+                    InlineKeyboardButton("Cancel🚫", callback_data = "closeme")
                 ]
             ]
         )
@@ -38,12 +38,12 @@ async def progress_for_pyrogram(current, total, ud_type, message, start):
         elapsed_time = TimeFormatter(milliseconds=elapsed_time)
         estimated_total_time = TimeFormatter(milliseconds=estimated_total_time)
 
-        progress = "[{0}{1}] \n <b>📊Percentage:</b> {2}%\n".format(
+        progress = "[{0}{1}] \n <b>Percentage📊:</b> {2}%\n".format(
             ''.join(["■" for i in range(math.floor(percentage / 5))]),
             ''.join(["□" for i in range(20 - math.floor(percentage / 5))]),
             round(percentage, 2))
 
-        tmp = progress + "<b>✅Completed:</b>{0} \n<b>📁Total Size:</b> {1}\n<b>🚀Speed:</b> {2}/s\n<b>⌚️ETA:</b> {3}\n".format(
+        tmp = progress + "<b>✅Completed:</b>{0} \n<b>Total Size📁:</b> {1}\n<b>Speed🚀:</b> {2}/s\n<b>⌚️ETA:</b> {3}\n".format(
             humanbytes(current),
             humanbytes(total),
             humanbytes(speed),
