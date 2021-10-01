cafe1d6a795736c52fc1a5867b5086b5b90f2924
RajYeagerist <91699154+RajYeagerist@users.noreply.github.com>
2021-10-01 14:40:33 +0530

    Sigma

--- translation.py
+++ translation.py
@@ -1,23 +1,23 @@
 class Translation(object):
-    START_TEXT = """ <b>OwO Hey There {}, \nI'm Ai Hayasaka VN's Waifu & A Bot, I can Rename+Convert Multiple Files At Once With Permanent Thumbnail And Custom Caption support Yay! 😁</b> \n
+    START_TEXT = """ <b>Hello {}, \nI'm [Chizuru Hishiro](https://telegra.ph/file/2aef498ea2913c3cc4318.jpg)...Guess What? I'm Funny, And I can Even Rename+Convert Multiple Files At Once With Permanent Thumbnail And Custom Caption support Sounds Cool right? </b> \n
 
-<b>I'm Well Maintained By: @Vedant_vn 👀✌</b> \n 
+<b>I'm Maintained By: @Vedant_vn 😈</b> \n 
 """
 
-    BANNED_USER_TEXT = "Lol My Owner Has Banned You From Using Me. That Means You Can't Run Me Now! \n \n Contact : @NarutoRobot_Support To Know More..." 
-    DOWNLOAD_START = "<b>Oh ya Baby OvO, Downloading Your File To My server! Just Wait a Bit</b>"
-    UPLOAD_START = "<b>Downloading Is Now Completed,Okay I'm Uploading Your File into Our PM 😉</b>"
-    AFTER_SUCCESSFUL_UPLOAD_MSG = "<b>Heya Thank you for Using Me Support Our Channel @AnimeSigma❤️</b>"
-    SAVED_CUSTOM_THUMB_NAIL = "<b>thumbnail Saved ✅ This Is Permanent Until</b> /delthumb ❤"
-    DEL_ETED_CUSTOM_THUMB_NAIL = "Ok Reeee...thumbnail cleared succesfully🤦 Khush Abb?"
-    SAVED_RECVD_DOC_FILE = "<b>Yay File Downloaded Successfully 😎</b>"
-    REPLY_TO_DOC_FOR_RENAME_FILE = "<b>Oi Noob Baka Reply with /rename file name & extension To rename a file</b>"
-    REPLY_TO_FILE_FOR_CONVERT = "<b>Hmm Reply To An File With /c2v To Convert It Into Streamable video File</b>"
+    BANNED_USER_TEXT = "Looks Like My Owner Has Banned You From Using Me. That Means You Can't Run Me Now! \n \n Contact : @NarutoRobot_Support To Know More..." 
+    DOWNLOAD_START = "<b>Umm okay, I'm Downloading Your File To My server! have Patience Young One 😸</b>"
+    UPLOAD_START = "<b>Downloading Is Now Completed,Okay I'm Uploading Your File into This PM ☺</b>"
+    AFTER_SUCCESSFUL_UPLOAD_MSG = "<b>Nice Of You To Use ME, Now Support Our Channel @AnimeSigma❤️</b>"
+    SAVED_CUSTOM_THUMB_NAIL = "<b>thumbnail Saved ✅ This Is Permanent Until</b> /delthumb 🤍"
+    DEL_ETED_CUSTOM_THUMB_NAIL = "Okay Sir...thumbnail cleared succesfully"
+    SAVED_RECVD_DOC_FILE = "<b>And File is Downloaded Successfully ♻</b>"
+    REPLY_TO_DOC_FOR_RENAME_FILE = "<b>Simple Minded Customer Please Reply with /rename file name & extension To rename a file</b>"
+    REPLY_TO_FILE_FOR_CONVERT = "<b Hey Reply To An File With /c2v To Convert It Into Streamable video File</b>"
     CUSTOM_CAPTION_UL_FILE = " "
-    NO_THUMB_FOUND = "Aur Bhai Thumbnail Kab Daaloge?"
-    IFLONG_FILE_NAME = """Oi Pagal...Decrease The Number Of Letters,Chutiya Salla 🙂"""
-    ABOUT_ME = """<b>UwU \nHey I'm Ai Hayasaka 🙈 Just Another Telegram File Renamer Bot Which Can Rename Telegram Files. \nWell,I Support Permanent Thumbnail Feature.\n \ni can also convert files into videos \nStill Got Any Issues With Me HUH?🤨 Contact @NarutoRobot_Support & Tag @Vedant_Vn there For Your Answers 🙂 </b>"""
-    HELP_USER = """Checkout The Available Commands Here \n\nDo Join @AnimeSigma If You Found Me Useful Darling❤️"""
+    NO_THUMB_FOUND = "When Will You Set Thumbnail? 🙂"
+    IFLONG_FILE_NAME = """Ayee...Decrease The Number Of Letters"""
+    ABOUT_ME = """<b>Ahh Hi? \nHey I'm Hishiro Chizuru Another Telegram File Renamer Bot Which Can Rename Telegram Files. \nAnd,I Support Permanent Thumbnail Feature.\n \nalso I can convert files into videos \nStill Got Complaints 😖 Contact @NarutoRobot_Support & Tag @Vedant_Vn there For Your Answers 🙂 </b>"""
+    HELP_USER = """Checkout The Available Commands Here \n\nJoin @AnimeSigma If You Need a Friendly Hug! 👉🏻👈🏻😳"""
     RENAME_HELP = """Here are The Available Commands In Rename \n\n\n▪️ <code>/rename</code> : Reply To An File/video With <code>/rename Filename.extension</code> For Renaming"""
     C2V_HELP = """Here Are The Available Commands In File To Video \n\n\n ▪️<code>/c2v</code> : Reply To An File With /c2v To Convert It Into Video"""
     THUMBNAIL_HELP = """Here Are The Available Commands In Custom Thumbnail \n\n\n ▪️ Send A Photo To Set The Custom Thumbnail \n▪️ <code>/showthumb</code> : For Checking The Current Thumbnail \n▪️<code>/delthumb</code> : For Deleting The Current Saved Thumbnail"""
